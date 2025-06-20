0ef6e2bc28bbb8157ce9926ea4bc7fb00182585a
Santhosh Nantha <sandynanda666@gmail.com>
2025-06-21 00:32:28 +0530

    Fix route field and return full Render URL

--- routes/url.js
+++ routes/url.js
@@ -4,40 +4,31 @@ const { nanoid } = require("nanoid");
 const validator = require("validator");
 const Url = require("../models/Url");
 
-
-router.post("/", async (req, res) => {
+// POST /shorten
+router.post("/shorten", async (req, res) => {
   const { url } = req.body;
 
-  
-  if (!url) {
-    return res.status(400).json({ error: "URL is required" });
-  }
+  if (!url) return res.status(400).json({ error: "URL is required" });
 
-  
   if (!validator.isURL(url)) {
     return res.status(400).json({ error: "Invalid URL format" });
   }
 
-  
   const shortId = nanoid(7);
+  await Url.create({ shortId, longUrl: url }); // longUrl must match model
 
-  
-  await Url.create({ shortId, longUrl: url });
-
-  
   res.json({
-    shortUrl: `https://url-shortner-e9km.onrender.com/url/${shortId}`
+    shortUrl: `https://url-shortner-e9km.onrender.com/${shortId}`
+    // or use `https://short.ly/${shortId}` for internship format
   });
 });
 
-// GET /:shortId
-router.get("/:shortId", async (req, res) => {
-  const { shortId } = req.params;
+// GET /:code
+router.get("/:code", async (req, res) => {
+  const { code } = req.params;
 
-  const entry = await Url.findOne({ shortId });
-  if (!entry) {
-    return res.status(404).send("Short URL not found");
-  }
+  const entry = await Url.findOne({ shortId: code });
+  if (!entry) return res.status(404).send("Short URL not found");
 
   res.redirect(entry.longUrl);
 });
