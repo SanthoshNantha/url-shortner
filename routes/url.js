65d9123aa8fc41900f9562a00183a95d79ca8b62
Santhosh Nantha <sandynanda666@gmail.com>
2025-06-21 00:18:03 +0530

    Added URL validation using validator

--- routes/url.js
+++ routes/url.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const { nanoid } = require("nanoid");
+const validator = require("validator");
 const Url = require("../models/Url");
 
-// POST: Create short URL
-router.post("/", async (req, res) => {
-    const { longUrl } = req.body;
-    const shortId = nanoid(8);
 
-    await Url.create({ shortId, longUrl });
-    res.json({ shortId });
+router.post("/", async (req, res) => {
+  const { url } = req.body;
+
+  
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  
+  if (!validator.isURL(url)) {
+    return res.status(400).json({ error: "Invalid URL format" });
+  }
+
+  
+  const shortId = nanoid(7);
+
+  
+  await Url.create({ shortId, longUrl: url });
+
+  
+  res.json({
+    shortUrl: `https://url-shortner-e9km.onrender.com/url/${shortId}`
+  });
 });
 
-// GET: Redirect to long URL
+// GET /:shortId
 router.get("/:shortId", async (req, res) => {
-    const { shortId } = req.params;
-    const entry = await Url.findOne({ shortId });
+  const { shortId } = req.params;
 
-    if (!entry) return res.status(404).send("Not found");
-    res.redirect(entry.longUrl);
+  const entry = await Url.findOne({ shortId });
+  if (!entry) {
+    return res.status(404).send("Short URL not found");
+  }
+
+  res.redirect(entry.longUrl);
 });
 
 module.exports = router;
